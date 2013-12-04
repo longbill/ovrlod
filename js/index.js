b393ce3495f8c360a28497d04a344dea40f56f1e
longbill.cn@gmail.com <liuchun643210>
2013-12-04 23:07:56 +0800

    a

--- js/index.js
+++ js/index.js
@@ -24,6 +24,88 @@ $(function()
     	},
     	smoothScrolling:true
     });
+
+
+
+
+	(function()
+	{
+		var $wrapper = $('#work .workslide');
+		var $ul = $wrapper.children('ul');
+		var $next = $('#work .handler .next');
+		var $prev = $('#work .handler .prev');
+		var $item = $ul.children('li').eq(0);
+		var width = $item.width();
+		var Width = $wrapper.width();
+
+		var windowItems = Math.round(Width/width);
+		var totalItems = $ul.children('li').length;
+
+		$ul.css('left','0px').data('current',0);
+
+		check_availability();
+
+
+		$next.mousedown(function(evt)
+		{
+			evt.preventDefault();
+			evt.stopPropagation();
+			var currentLeft = get_currentLeft();
+			var newLeft = currentLeft + windowItems;
+			if (newLeft +  windowItems >= totalItems ) newLeft = totalItems - windowItems;
+			slideTo(newLeft);
+		});
+
+		$prev.mousedown(function(evt)
+		{
+			evt.preventDefault();
+			evt.stopPropagation();
+			var currentLeft = get_currentLeft();
+			var newLeft = currentLeft - windowItems;
+			if (newLeft < 0) newLeft = 0;
+			slideTo(newLeft);
+		});
+
+		function slideTo(i)
+		{
+			$ul.animate({ left : -1*i*width },300,function()
+			{
+				$ul.data('current',i);
+				check_availability();
+			});
+		}
+
+		function get_currentLeft()
+		{
+			var i = $ul.data('current');
+			if (!i) i = 0;
+			return i;
+		}
+
+		function check_availability()
+		{
+			var currentLeft = get_currentLeft();
+			var currentRight = currentLeft + windowItems;
+
+			if (currentLeft <= 0)
+				$prev.addClass('disabled');
+			else
+				$prev.removeClass('disabled');
+
+			if (currentRight >= totalItems)
+				$next.addClass('disabled');
+			else
+				$next.removeClass('disabled');
+		}
+
+	})();
+	
+
+
+
+
+
+
 });
 
 
